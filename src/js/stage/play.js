e535e52c9bd32f738931c6ba02013b7368e6fa3b
Daniel Garrido <130117760+danielgarrido1@users.noreply.github.com>
2023-12-06 14:29:34 -0800

    Add files via upload

---
+++ src/js/stage/play.js
@@ -0,0 +1,33 @@
+import * as me from 'melonjs/dist/melonjs.module.js';
+
+import HUD from '../renderables/hud/container.js'
+import data from '../data.js'
+
+export default class PlayScreen extends me.Stage {
+
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent() {
+
+		// load a level
+		me.level.load("area01");
+
+		// reset the score
+		data.score = 0;
+
+		// add our HUD to the game world
+		this.HUD = new HUDContainer();
+		me.game.world.addChild(this.HUD);
+
+	}
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+}
---
+++ src/manifest.js
@@ -0,0 +1,10 @@
+// a melonJS data manifest
+const DataManifest = [
+
+    /* Bitmap Text */
+{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
+{name: "area01",              type: "tmx",	src: "data/map/area01.tmx"}
+   
+];
+
+export default DataManifest;
